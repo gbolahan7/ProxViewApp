2fff97f3a72a99f17acb7b29dfcb900e64b18897
Abass <abass_gbolahan@yahoo.com>
2022-02-16 22:38:31 +0000

    Login UI screen created

---
+++ app/src/main/java/uk/ac/tees/b1110843/proxviewapp/Activity/SplashActivity.java
@@ -0,0 +1,16 @@
+package uk.ac.tees.b1110843.proxviewapp.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import uk.ac.tees.b1110843.proxviewapp.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
